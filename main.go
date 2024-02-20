40bd625901b78ac69f4f9896c3772cf06eac59e0
DavidDiasN <daviddiasnogueira@gmail.com>
2024-02-19 23:56:22 -0500

    Familiarizing myself with the basics of pgx

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5"
+	"log"
+	"strings"
 	//	"log"
 	//	s "main/server"
 	//	"net/http"
 	"os"
 )
+
+/*
+Make PostgresPlayerStore
+I am thinking we will need to store a connection?
+I will need to read up on how to do this since working with databases
+in this way will be new to me.
+How many connections do I want to be able to support?
+I have a lot of questions to ask myself and there is a lot to implement here.
+
+
+*/
 
 type InMemoryPlayerStore struct {
 	store map[string]int
@@ -31,20 +45,42 @@ func main() {
 	//	myServer := &s.PlayerServer{Store: NewInMemoryPlayerStore()}
 	//	log.Fatal(http.ListenAndServe(":5000", myServer))
 
-	conn, err := pgx.Connect(context.Background(), os.Getenv("WEB_APP_DB"))
-	// conn, err := pgx.Connect(context.Background(), "WEB_APP_DB")
+	rdr := bufio.NewReader(os.Stdin)
+	userInput, _ := rdr.ReadString('\n')
+	userInput = strings.TrimSuffix(userInput, "\n")
+
+	queryTemplate := fmt.Sprintf(`SELECT gamesWon FROM scores WHERE name = '%s';`, userInput)
+
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, os.Getenv("WEB_APP_DB"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
+	//queryResult, err := conn.Exec(ctx, `INSERT INTO scores (name, gamesWon)
+	//VALUES ('Jerry', 5);`)
+
+	//fmt.Println(queryResult.String())
+
+	var numbers []int32
+
+	rows, err := conn.Query(ctx, queryTemplate)
+	for rows.Next() {
+		var gamesWon int32
+		err = rows.Scan(&gamesWon)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, gamesWon)
 	}
 
-	fmt.Println(greeting)
+	if rows.Err() != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(numbers)
+
 }
