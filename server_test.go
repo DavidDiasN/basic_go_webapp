50d1b22d7a3c25461464e239ee623a1c4923b1e4
DavidDiasN <daviddiasnogueira@gmail.com>
2024-03-09 13:53:26 -0500

    adjusted poker

--- server_test.go
+++ server_test.go
@@ -88,14 +88,7 @@ func TestStoreWins(t *testing.T) {
 		myServer.ServeHTTP(response, request)
 
 		assertResponseHeader(t, response.Code, http.StatusAccepted)
-
-		if len(store.winCalls) != 1 {
-			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
+    assertPlayerWin(t, &store, player)
 	})
 }
 
@@ -174,4 +167,17 @@ func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want s
 	if response.Result().Header.Get("content-type") != "application/json" {
 		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
 	}
+}
+
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+  t.Helper()
+
+
+  if len(store.winCalls) != 1 {
+    t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+  }
+
+  if store.winCalls[0] != winner {
+    t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+  }
 }
