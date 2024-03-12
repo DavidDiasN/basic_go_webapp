7151bd74c2a2405825a57bad7fde076f4c1d8b1a
DavidDiasN <daviddiasnogueira@gmail.com>
2024-03-12 11:49:35 -0400

    please no problems, ty

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
 
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	db, closeDB, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
 		log.Fatalf("Problem opening %s %v", dbFileName, err)
